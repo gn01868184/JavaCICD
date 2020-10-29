167f54b1eb54a7e02d8af552aa7fca21271bbd00
gn01868184 <gn01868184@yahoo.com.tw>
2020-10-29 15:54:12 +0800

    Update HelloWorld.java

--- src/main/java/ntou/cs/HelloWorld.java
+++ src/main/java/ntou/cs/HelloWorld.java
@@ -1,32 +1,26 @@
 package ntou.cs;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.*;
 
-/**
- * Servlet implementation class HelloWorld
- */
- public class HelloWorld extends HttpServlet {
+public class HelloWorld extends HttpServlet {
 
-     @Override
-     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-             throws ServletException, IOException {
-         resp.getWriter().print("Hello from Java!\n");
-     }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.getWriter().print("Hello from Java!\n");
+    }
 
-     public static void main(String[] args) throws Exception{
-         Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-         context.setContextPath("/");
-         server.setHandler(context);
-         context.addServlet(new ServletHolder(new HelloWorld()),"/*");
-         server.start();
-         server.join();
-     }
- }
+    public static void main(String[] args) throws Exception{
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new HelloWorld()),"/*");
+        server.start();
+        server.join();
+    }
+}
