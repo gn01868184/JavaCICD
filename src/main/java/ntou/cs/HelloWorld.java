6021c728ef31e2b71aa9d1728e20792989e64e13
gn01868184 <gn01868184@yahoo.com.tw>
2020-11-06 10:22:59 +0800

    Update HelloWorld.java

--- src/main/java/ntou/cs/HelloWorld.java
+++ src/main/java/ntou/cs/HelloWorld.java
@@ -14,7 +14,39 @@ public class HelloWorld extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
-        writer.println("<h1>Hello</h1>");
+        writer.println("<head>
+            <meta charset="utf-8">
+            <title>Basic DOM</title>
+            <style type="text/CSS">
+                @import url(https://fonts.googleapis.com/earlyaccess/cwtexyen.css);
+
+                h1 {
+        			font-size: 50px;
+        			font-family: cwTeXYen;
+        		}
+
+        		div {
+        			font-size: 80px;
+        			font-family: Consolas;
+        			margin: 5px;
+        			float: left;
+        		}
+        		div.number {
+        			border: 1px solid black;
+        			color: dodgerblue;
+        		}
+        	</style>
+        </head>");
+        writer.println("<body>
+            <h1>四則運算結果:</h1>
+            <div class="number" id="num1">Number1</div>
+            <div id="op">+</div>
+            <div class="number" id="num2">Number2</div>
+            <div>=</div>
+            <div class="number" id="result">Result</div>
+        </body>");
+
+
         writer.close();
     }
 
