0fdb32bf1bc6b8b248e53f4e9527cff2be9d577e
gn01868184 <gn01868184@yahoo.com.tw>
2020-11-06 10:34:54 +0800

    Update HelloWorld.java

--- src/main/java/ntou/cs/HelloWorld.java
+++ src/main/java/ntou/cs/HelloWorld.java
@@ -14,36 +14,36 @@ public class HelloWorld extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
-        writer.println('<head>');
-        writer.println('<meta charset="utf-8">');
-        writer.println('<title>Basic DOM</title>');
-        writer.println('<style type="text/CSS">');
-        writer.println('@import url(https://fonts.googleapis.com/earlyaccess/cwtexyen.css);');
-        writer.println('h1 {');
-        writer.println('font-size: 50px;');
-        writer.println('font-family: cwTeXYen;');
-        writer.println('}');
-        writer.println('div {');
-        writer.println('font-size: 80px;');
-        writer.println('font-family: Consolas;');
-        writer.println('margin: 5px;');
-        writer.println('float: left;');
-        writer.println('}');
-        writer.println('div.number {');
-        writer.println('border: 1px solid black;');
-        writer.println('color: dodgerblue;');
-        writer.println('}');
-        writer.println('</style>');
-        writer.println('</head>');
+        writer.println("<head>");
+        writer.println("<meta charset='utf-8'>");
+        writer.println("<title>Basic DOM</title>");
+        writer.println("<style type='text/CSS'>");
+        writer.println("@import url(https://fonts.googleapis.com/earlyaccess/cwtexyen.css);");
+        writer.println("h1 {");
+        writer.println("font-size: 50px;");
+        writer.println("font-family: cwTeXYen;");
+        writer.println("}");
+        writer.println("div {");
+        writer.println("font-size: 80px;");
+        writer.println("font-family: Consolas;");
+        writer.println("margin: 5px;");
+        writer.println("float: left;");
+        writer.println("}");
+        writer.println("div.number {");
+        writer.println("border: 1px solid black;");
+        writer.println("color: dodgerblue;");
+        writer.println("}");
+        writer.println("</style>");
+        writer.println("</head>");
 
-        writer.println('<body>');
-        writer.println('<h1>四則運算結果:</h1>');
-        writer.println('<div class="number" id="num1">Number1</div>');
-        writer.println('<div id="op">+</div>');
-        writer.println('<div class="number" id="num2">Number2</div>');
-        writer.println('<div>=</div>');
-        writer.println('<div class="number" id="result">Result</div>');
-        writer.println('</body>');
+        writer.println("<body>");
+        writer.println("<h1>四則運算結果:</h1>");
+        writer.println("<div class='number' id='num1'>Number1</div>");
+        writer.println("<div id='op'>+</div>");
+        writer.println("<div class='number' id='num2'>Number2</div>");
+        writer.println("<div>=</div>");
+        writer.println("<div class='number' id='result'>Result</div>");
+        writer.println("</body>");
 
         writer.close();
     }
